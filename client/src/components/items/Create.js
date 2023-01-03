015d9f475d6ddca3a64f4912fe49bd2264380f71
MOUATAZ HAKKOU <40317134+mouataz1@users.noreply.github.com>
2023-01-03 23:21:27 +0100

    create new item and redirect

--- client/src/components/items/Create.js
+++ client/src/components/items/Create.js
@@ -1,6 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
 function Create() {
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [image, setImage] = useState("");
+    const navigete = useNavigate();
+
+    const handleForme = async e => {
+        e.preventDefault();
+        try {
+            const body = {
+                "title": title,
+                "description": description,
+                "image": image
+            }
+            const res = await axios.post("http://localhost:5000/items", body);
+            if(res.status == 200){
+                //alert("item created successfully");
+                navigete("/");
+            }
+            
+            
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
+
   return (
     <div className='container pt-5 p-3'>
         <div className="card p-3" >
@@ -10,19 +40,39 @@ function Create() {
                 </h1>
             </div>
             <div className="card-body">
-            <form>
+            <form onSubmit={handleForme}>
             <div className="mb-3">
                 <label for="exampleInputEmail1" className="form-label">Item Title</label>
-                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <input type="text"
+                 className="form-control"
+                 value={title}
+                 onChange={e => setTitle(e.target.value)}
+                  id="exampleInputEmail1"
+                   aria-describedby="emailHelp"
+                    />
                 
             </div>
             <div className="mb-3">
                 <label for="exampleInputPassword1" className="form-label">Item description</label>
-                <textarea className="form-control" name="" id="" cols="30" rows="5"></textarea>
+                <textarea
+                 className="form-control"
+                 value={description}
+                 onChange={e => setDescription(e.target.value)}
+                 name="" 
+                 id="" 
+                 cols="30" 
+                 rows="5"
+                 ></textarea>
             </div>
             <div className="mb-3">
             <label for="file" className="form-label">Item Image</label>
-                <input type="file" name="" id="fole" className="form-control" />
+                <input 
+                type="text" 
+                value={image}
+                onChange={e => setImage(e.target.value)}
+                name="" 
+                id="fole" 
+                className="form-control" />
             </div>
             <div className="mb-3 d-flex justify-content-center aligne-items-center">
                 <button type="submit" className="btn  btn-success">Submit</button>
--- client/src/components/items/List.js
+++ client/src/components/items/List.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 function List() {
   return (
     <div className="container pt-3">
